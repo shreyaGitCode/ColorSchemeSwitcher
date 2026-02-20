e3dfe5cd59ca0e9badcc53e287f77389913771f4
KUMARI SHREYA <144919563+shreyaGitCode@users.noreply.github.com>
2026-02-20 23:23:08 +0530

    Implement button click events for color switching
    Add event listeners to buttons for background color change.

--- bcgColorSwitcher.js
+++ bcgColorSwitcher.js
@@ -1,1 +1,14 @@
+const buttons = document.querySelectorAll(".button")
+const body = document.querySelector("body")
 
+buttons.forEach(function(button){
+	console.log(button);
+	button.addEventListener('click', function(event){
+		console.log(event);
+		console.log(event.target);
+		if(event.target.id === "red"){
+			body.style.backgroundColor = "event.target";
+		}
+	});
+});
+  
