0a903a2156d0310d181144a26a3cbfeb9807dcce
KUMARI SHREYA <144919563+shreyaGitCode@users.noreply.github.com>
2026-02-22 03:21:05 +0530

    Implement button click color change functionality
    Add event listeners to buttons for color changes

--- JS2.js
+++ JS2.js
@@ -1,1 +1,26 @@
+const buttons = document.querySelectorAll(".button")
+const body = document.querySelector("body")
 
+buttons.forEach(button => {
+    button.addEventListener("click", (event) =>{
+        body.style.color = event.target.id;
+        if(event.target.id === "red"){
+            body.style.backgroundColor = "rgb(128,5,5)";
+        }
+        if(event.target.id === "blue"){
+            body.style.backgroundColor = "rgb(8,112,150)";
+        }
+        if(event.target.id === "yellow"){
+            body.style.backgroundColor = "rgb(175,175,8)";
+        }
+        if(event.target.id === "purple"){
+            body.style.backgroundColor = "rgb(116,11,116)";
+        }
+        if(event.target.id === "white"){
+            body.style.backgroundColor = "white";
+        }
+        if(event.target.id === "black"){
+            body.style.backgroundColor = "black";
+        }
+    });
+});
